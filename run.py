d32208fdcb0ef45a5ee149783ed487c2c9e46fd7
Hesam Sheikh <hessamshekhasany@gmail.com>
2024-11-07 22:05:32 +0100

    updated minizinc with more optimizations, added LB

--- run.py
+++ run.py
@@ -20,6 +20,9 @@ def parse_output_into_json(output_string):
     upper_bound_match = re.search(r'The Upper Bound: (\d+)', output_string)
     upper_bound = int(upper_bound_match.group(1)) if upper_bound_match else None
 
+    lower_bound_match = re.search(r'The Lower Bound: (\d+)', output_string)
+    lower_bound = int(lower_bound_match.group(1)) if lower_bound_match else None
+
     solve_time_match = re.search(r'%%%mzn-stat: solveTime=([\d.]+)', output_string)
     solve_time = float(solve_time_match.group(1)) if solve_time_match else None
 
@@ -28,20 +31,21 @@ def parse_output_into_json(output_string):
         "time": time_elapsed,
         "solve_time": solve_time,
         "upper_bound": upper_bound,
+        "lower_bound": lower_bound,
         "optimal": False if time_elapsed > 300 else True,
         "obj": max_distance,
         "sol": courier_routes,
-    }
+    }    
     return result
 
-def run_subprocess(instance_path, model):
+def run_subprocess(instance_path, model, solver_type="gecode"):
     try:
         output = subprocess.run(
             [
                 "minizinc", 
                 "-m", model, 
                 "-d", instance_path,
-                "--solver", 'gecode', 
+                "--solver", solver_type, 
                 "--output-time", 
                 "--solver-time-limit", "300000",
                 "-s" 
@@ -49,7 +53,7 @@ def run_subprocess(instance_path, model):
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            shell=False,
+            shell=False, 
             timeout=301,
         )
         return output.stdout
@@ -58,40 +62,49 @@ def run_subprocess(instance_path, model):
         return None
 
 
-def solve_instance_cp(models, instance_path):
+def solve_instance_cp(models, instance_path, solver_type="gecode"):
     output_json = {}
 
     for model in models:
-        output_string = run_subprocess(instance_path, model)
+        output_string = run_subprocess(instance_path, model, solver_type)
         if output_string:
             output_json[model] = parse_output_into_json(output_string)
 
     return output_json
 
-instances_dir = "instances_dzn"
-output_dir = "json_output2"
-models = [
-    "CP_SYM_UPB.mzn",
-    "CP_SYM_LINUPB.mzn"
-    ]  # List of solvers to run
-
-
-# Ensure the output directory exists
-os.makedirs(output_dir, exist_ok=True)
-
-# Iterate over each file in the instances directory
-for instance_file in os.listdir(instances_dir):
-    if not instance_file.endswith(".dzn"):
-        print("File type must be .dzn.")
-        continue
-
-    instance_path = os.path.join(instances_dir, instance_file)
-    output_json = solve_instance_cp(models, instance_path)
-
+def save_json_output(json_data):
     # Save the JSON output to the output directory
     json_filename = f"{os.path.splitext(instance_file)[0]}.json"
     json_path = os.path.join(output_dir, json_filename)
     with open(json_path, 'w') as json_file:
-        json.dump(output_json, json_file, indent=4)
+        json.dump(json_data, json_file, indent=4)
+
+
+if __name__=="__main__":
+    instances_dir = "instances_dzn"
+    output_dir = "outputs/3_solvers_outputs"
+    models = [
+        "CP_SYM_UPB.mzn",
+        "CP_SYM_LINUPB.mzn",
+        "CP_SYM_VER2.mzn",
+        ]
+    solver_type = "chuffed"
+
+
+    # Ensure the output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Iterate over each file in the instances directory
+    for instance_file in os.listdir(instances_dir)[:11]:
+        if not instance_file.endswith(".dzn"):
+            print("File type must be .dzn.")
+            continue
+        instance_path = os.path.join(instances_dir, instance_file)
+        
+        print(f"Solving {instance_file}")
+        output_json = solve_instance_cp(models, instance_path, solver_type)
+
+        if output_json:
+            save_json_output(output_json)
 
 ...
